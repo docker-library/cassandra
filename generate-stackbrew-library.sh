d8595fb29eec2038ff98505e66ee1757d94fe423
Tianon Gravi <admwiggin@gmail.com>
2016-01-26 09:43:58 -0800

    Fix duplicates

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -18,7 +18,11 @@ echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 for version in "${versions[@]}"; do
 	commit="$(cd "$version" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 	fullVersion="$(grep -m1 'ENV CASSANDRA_VERSION ' "$version/Dockerfile" | cut -d' ' -f3 | cut -d- -f1)"
-	versionAliases=( $fullVersion $version ${aliases[$version]} )
+	versionAliases=( $fullVersion )
+	if [ "$version" != "$fullVersion" ]; then
+		versionAliases+=( $version )
+	fi
+	versionAliases+=( ${aliases[$version]} )
 	
 	echo
 	for va in "${versionAliases[@]}"; do
