1f1c4fe481e9335adf2eab968531ea436e31359c
Richard Mathie <richard.mathie@amey.co.uk>
2016-08-02 13:41:17 +0100

    This should now work. added config for cqlsh testing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,72 +3,78 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1:0:1}" = '-' ]; then
-	set -- cassandra -f "$@"
+  set -- cassandra -f "$@"
 fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'cassandra' -a "$(id -u)" = '0' ]; then
-	chown -R cassandra /var/lib/cassandra /var/log/cassandra "$CASSANDRA_CONFIG"
-	exec gosu cassandra "$BASH_SOURCE" "$@"
+  chown -R cassandra /var/lib/cassandra /var/log/cassandra "$CASSANDRA_CONFIG"
+  exec gosu cassandra "$BASH_SOURCE" "$@"
 fi
 
 if [ "$1" = 'cassandra' ]; then
 
   sleep $[ ( $RANDOM % 10 ) + 1]
 
-	: ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
+  : ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
   : ${HOST_COMMAND='hostname --ip-address'}
+
+  : ${SEEDS_COMMAND="echo \$CASSANDRA_BROADCAST_ADDRESS"}
+
+  # Listen Address
   : ${CASSANDRA_LISTEN_ADDRESS_COMMAND=$HOST_COMMAND}
-  : ${SEEDS_COMMNAD="echo \$CASSANDRA_BROADCAST_ADDRESS"}
+  : ${CASSANDRA_LISTEN_ADDRESS='auto'}
+  if [ "$CASSANDRA_LISTEN_ADDRESS" = 'auto' ]; then
+    CASSANDRA_LISTEN_ADDRESS=$(eval $CASSANDRA_LISTEN_ADDRESS_COMMAND)
+  fi
 
-	: ${CASSANDRA_LISTEN_ADDRESS='auto'}
-	if [ "$CASSANDRA_LISTEN_ADDRESS" = 'auto' ]; then
-		CASSANDRA_LISTEN_ADDRESS=$(eval $CASSANDRA_LISTEN_ADDRESS_COMMAND)
-	fi
-
-	: ${CASSANDRA_BROADCAST_ADDRESS="$CASSANDRA_LISTEN_ADDRESS"}
+  # Broadcast Address
   : ${CASSANDRA_BROADCAST_ADDRESS_COMMAND=$HOST_COMMAND}
+  : ${CASSANDRA_BROADCAST_ADDRESS="$CASSANDRA_LISTEN_ADDRESS"}
 
-	if [ "$CASSANDRA_BROADCAST_ADDRESS" = 'auto' ]; then
-		CASSANDRA_BROADCAST_ADDRESS=$(eval $CASSANDRA_BROADCAST_ADDRESS_COMMAND)
-	fi
-	: ${CASSANDRA_BROADCAST_RPC_ADDRESS:=$CASSANDRA_BROADCAST_ADDRESS}
+  if [ "$CASSANDRA_BROADCAST_ADDRESS" = 'auto' ]; then
+    CASSANDRA_BROADCAST_ADDRESS=$(eval $CASSANDRA_BROADCAST_ADDRESS_COMMAND)
+  fi
+  : ${CASSANDRA_BROADCAST_RPC_ADDRESS:=$CASSANDRA_BROADCAST_ADDRESS}
 
-	if [ -n "${CASSANDRA_NAME:+1}" ]; then
-		: ${CASSANDRA_SEEDS:="cassandra"}
-	fi
+  # Seeds
+  if [ -n "${CASSANDRA_NAME:+1}" ]; then
+    : ${CASSANDRA_SEEDS="cassandra"}
+  fi
 
   if [ "$CASSANDRA_SEEDS" = 'auto' ]; then
-	  CASSANDRA_SEEDS=$(eval $SEEDS_COMMNAD)
-	fi
-	
-	sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
+    echo "evaluating cassandra seeds with $SEEDS_COMMAND"
+    CASSANDRA_SEEDS=$(eval $SEEDS_COMMAND)
+  fi
+  
+  sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
 
-	for yaml in \
-		broadcast_address \
-		broadcast_rpc_address \
-		cluster_name \
-		endpoint_snitch \
-		listen_address \
+  # Substitution
+  for yaml in \
+    broadcast_address \
+    broadcast_rpc_address \
+    cluster_name \
+    endpoint_snitch \
+    listen_address \
     listen_interface \
-		num_tokens \
-		rpc_address \
-		start_rpc \
-	; do
-		var="CASSANDRA_${yaml^^}"
-		val="${!var}"
-		if [ "$val" ]; then
-			sed -ri 's/^(# )?('"$yaml"':).*/\2 '"$val"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
-		fi
-	done
+    num_tokens \
+    rpc_address \
+    start_rpc \
+  ; do
+    var="CASSANDRA_${yaml^^}"
+    val="${!var}"
+    if [ "$val" ]; then
+      sed -ri 's/^(# )?('"$yaml"':).*/\2 '"$val"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
+    fi
+  done
 
-	for rackdc in dc rack; do
-		var="CASSANDRA_${rackdc^^}"
-		val="${!var}"
-		if [ "$val" ]; then
-			sed -ri 's/^('"$rackdc"'=).*/\1 '"$val"'/' "$CASSANDRA_CONFIG/cassandra-rackdc.properties"
-		fi
-	done
+  for rackdc in dc rack; do
+    var="CASSANDRA_${rackdc^^}"
+    val="${!var}"
+    if [ "$val" ]; then
+      sed -ri 's/^('"$rackdc"'=).*/\1 '"$val"'/' "$CASSANDRA_CONFIG/cassandra-rackdc.properties"
+    fi
+  done
 fi
 
 exec "$@"
