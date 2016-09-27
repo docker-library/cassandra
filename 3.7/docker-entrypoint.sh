77d1a468a60923390e1c7bc5e16b08de0311e0f6
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-09-27 16:51:07 -0500

    make ports configurable

--- 3.7/docker-entrypoint.sh
+++ 3.7/docker-entrypoint.sh
@@ -43,6 +43,10 @@ if [ "$1" = 'cassandra' ]; then
 		num_tokens \
 		rpc_address \
 		start_rpc \
+		storage_port \
+		ssl_storage_port \
+		native_transport_port \
+		rpc_port \
 	; do
 		var="CASSANDRA_${yaml^^}"
 		val="${!var}"
